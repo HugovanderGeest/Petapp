f177a0252f5cd07796a3e0042fd3c5db5cb09091
Hugo van der Geest <hugo.van.der.geest.van.der.geest@hva.nl>
2024-03-18 15:59:52 +0100

    Update login page and reset database

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ def login():
             return redirect(url_for('dashboard'))
         else:
             flash('Invalid username or password', 'error')
-    return render_template('index.html')
+    return render_template('login.html')
 
 
 @app.route('/log_activity', methods=['POST'])
@@ -470,9 +470,9 @@ def assign_location(user_id):
     
     return render_template('assign_location.html', form=form)
 
-# with app.app_context():
-#     db.drop_all()
-#     db.create_all()
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)  

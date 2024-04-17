debf70e9fa485d31869829cc9a126de03fc4b408
Hugo van der Geest <hugo.van.der.geest.van.der.geest@hva.nl>
2024-04-18 00:47:50 +0200

    Refactor service worker and templates

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,19 +1,15 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('your-cache-name').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/static/style.css',
-                '/static/script.js'
-            ]);
-        })
-    );
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("your-cache-name").then(function (cache) {
+      return cache.addAll(["/", "/static/style.css", "/static/script.js"]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
 });
